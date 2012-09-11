010ae1674956a07656bf69127a866e2204606a47
Emanuel <Emanuel@localhost>
2012-09-11 10:23:50 -0300

    Initial commit

---
+++ FinalProject/gen/com/taller/finalproject/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.taller.finalproject;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ FinalProject/src/com/taller/finalproject/FinalProjectActivity.java
@@ -0,0 +1,31 @@
+package com.taller.finalproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FinalProjectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+
+	public void showWeather(View v){
+		int z = 4;
+		z++;
+	}
+	
+	public void showBattery(View v){
+		int z = 4;
+		z++;
+	}
+	
+	public void sendMail(View v){
+		int z = 4;
+		z++;
+	}
+
+}
